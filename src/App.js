0c06d58a81798238efab5fcfc2a0ee9369c5aa3d
William <william.helmrath@live.com>
2021-04-11 15:31:13 -0400

    added word to homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import firebase from "./firebase";
 
 const db = firebase.firestore();
 
 function App() {
+  const [word, setWord] = useState("");
   useEffect(() => {
     db.collection("users")
       .get()
       .then((res) => console.log(res));
   });
 
+  const handleChange = (event) => setWord(event.target.value);
+
   return (
-    <div style={{ textAlign: "center" }}>
-      <TextField label="Search" />
+    <div
+      style={{ textAlign: "center", display: "flex", flexDirection: "column" }}
+    >
+      <TextField label="Search" onChange={handleChange} value={word} />
+      {word}
     </div>
   );
 }
